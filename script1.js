da3ded1abcd955e2a47c507c474f9180d78a80e6
ted <theo_drz@live.com>
2022-02-21 13:43:26 -0500

    added basic arithmatic functions

--- script1.js
+++ script1.js
@@ -1,3 +1,32 @@
-console.log("doodoo")
 
- 
+function add(firstTerm, secondTerm) {
+    return firstTerm + secondTerm;
+}
+
+function subtract(firstTerm, secondTerm) {
+    return firstTerm - secondTerm
+}
+
+function multiply(firstTerm, secondTerm) {
+    return firstTerm * secondTerm
+}
+
+function divide(firstTerm, secondTerm) {
+    return firstTerm / secondTerm
+}
+
+function operate(firstTerm, operator, secondTerm) {
+    if (operator === "plus") {
+        return add(firstTerm, secondTerm);
+    } else if (operator === "minus") {
+        return subtract(firstTerm, secondTerm);
+    } else if (operator === "times") {
+        return multiply(firstTerm, secondTerm);
+    } else if (operator === "over") {
+        return divide(firstTerm, secondTerm);
+    } else {
+        return "error: didnt recognize operator"
+    }
+}
+
+console.log(operate(2,"times",10))
