f0acca039341701ad7e7dbf87ab213ac874bf172
ted <theo_drz@live.com>
2022-02-23 15:18:51 -0500

    reformatted, basic 2 term arithmatic works

--- script1.js
+++ script1.js
@@ -16,33 +16,94 @@ function divide(firstTerm, secondTerm) {
 }
 
 function operate(firstTerm, operator, secondTerm) {
-    if (operator === "plus") {
+    if (operator === "+") {
         return add(firstTerm, secondTerm);
-    } else if (operator === "minus") {
+    } else if (operator === "-") {
         return subtract(firstTerm, secondTerm);
-    } else if (operator === "times") {
+    } else if (operator === "*") {
         return multiply(firstTerm, secondTerm);
-    } else if (operator === "over") {
+    } else if (operator === "/") {
         return divide(firstTerm, secondTerm);
     } else {
         return "error: didnt recognize operator"
     }
 }
 
+let firstTerm = null;
+let secondTerm = null;
+let currentOperator = null;
+
+const displayVar = document.querySelector("#displayContainer")
+displayVar.textContent = null
+
+const numBtns = document.querySelectorAll(".number");
+for (const button of numBtns) {
+    button.addEventListener("click", () => {
+        //console.log(button.textContent)
+        displayVar.textContent = (displayVar.textContent).concat(button.textContent)
+    })
+}
+
+const funcBtns = document.querySelectorAll(".function")
+for (const button of funcBtns) {
+    button.addEventListener("click", () => {
+        //console.log(displayVar.textContent)
+        currentOperator = button.textContent;
+        if (firstTerm === null) {
+            firstTerm = displayVar.textContent;
+            displayVar.textContent = null;
+        } else if (secondTerm === null) {
+            secondTerm = displayVar.textContent;
+            displayVar.textContent = null;
+        }
+        if (firstTerm != null && secondTerm != null) {
+            console.log("YEET")
+            firstTerm = operate(Number(firstTerm), currentOperator, Number(secondTerm))
+            displayVar.textContent = firstTerm;
+        }
+        console.log("First Term is " + firstTerm)
+        console.log("operator is " + currentOperator)
+        console.log("second term is " + secondTerm)
+        //displayVar.textContent = null;
+    })
+}
+
+const equalsBtn = document.querySelector(".equals")
+equalsBtn.addEventListener("click", () => {
+    secondTerm = displayVar.textContent;
+    //console.log("first term: " + firstTerm)
+    //console.log("second term: " + secondTerm)
+    //console.log("operator is: " + currentOperator)
+    firstTerm = operate(Number(firstTerm), currentOperator, Number(secondTerm))
+    
+    displayVar.textContent = firstTerm
+})
+
+//OLD POOPOO
+
+//funcBtns.addEventListener("click", () => {
+//    console.log("doodoo")
+//})
+//numBtns.addEventListener("click", () => {
+//    console.log("YEET")
+//    return 0
+//})
+
 //console.log(operate(2,"times",10))
 
 
-const containerVar = document.querySelector("#container");
+//const containerVar = document.querySelector("#container");
 
-const display = document.createElement("div");
-display.classList.add("displayContainer");
-display.style.backgroundColor = "gray";
+//const display = document.createElement("div");
+//display.classList.add("displayContainer");
+//display.textContent = null
+//display.style.backgroundColor = "gray";
 
-const btnContainer = document.createElement("div");
-btnContainer.classList.add("btnContainer");
+//const btnContainer = document.createElement("div");
+//btnContainer.classList.add("btnContainer");
 
-containerVar.appendChild(display);
-containerVar.appendChild(btnContainer);
+//containerVar.appendChild(display);
+//containerVar.appendChild(btnContainer);
 
 function drawCalc() {
     let itemCounter = 0;
@@ -54,9 +115,13 @@ function drawCalc() {
 
         let rows = 0;
         while (rows < 5) {
-            const item = document.createElement("div");
+            const item = document.createElement("button");
             item.classList.add("btn");
             item.textContent = chars[itemCounter];
+            item.addEventListener("click", () => {
+                console.log(item.textContent)
+                display.textContent = (display.textContent).concat(item.textContent)
+            })
             columnContainer.appendChild(item);
             
             itemCounter++;
@@ -67,4 +132,4 @@ function drawCalc() {
     }
 }
 
-drawCalc()
+//drawCalc()
